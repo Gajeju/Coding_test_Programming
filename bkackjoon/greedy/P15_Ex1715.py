44ff1d0a33416962b3368998a1c9201ccb2ec816
Gajeju <qwert6603@naver.com>
2022-01-21 20:29:45 +0900

    1715 하는 중

---
+++ bkackjoon/greedy/P15_Ex1715.py
@@ -0,0 +1,19 @@
+n = int(input())
+card_list = [int(input()) for _ in range(n)]
+
+card_list.sort()
+
+
+
+print(cnt)
+
+
+
+
+# 카드 정렬하기
+# 정렬된 두 묶음의 숫자 카드 각 묶음의 카드의 수를 A, B
+# 두 묶음을 합치기 위해서는 A+B 번의 비교가 필요하다
+# N개의 묶음이 있을때 비교횟수 최소화
+
+
+# https://www.acmicpc.net/problem/1715
