c616a95302bd1aeb2ca1c5477fb50f41df72bb59
제주 제주 <qwert6603@naver.com>
2022-01-24 23:42:06 +0900

    greedy 1문제

--- bkackjoon/greedy/P15_Ex1715.py
+++ bkackjoon/greedy/P15_Ex1715.py
@@ -1,11 +1,21 @@
+import heapq
+
 n = int(input())
 card_list = [int(input()) for _ in range(n)]
+heapq.heapify(card_list)
 
-card_list.sort()
-
-
+cnt = 0
+cards = 0
+    
+while len(card_list) > 1:
+    a = heapq.heappop(card_list)
+    b = heapq.heappop(card_list)
+    cards = a + b
+    cnt += cards
+    heapq.heappush(card_list, cards)
 
 print(cnt)
+
 
 
 
@@ -15,5 +25,6 @@ print(cnt)
 # 두 묶음을 합치기 위해서는 A+B 번의 비교가 필요하다
 # N개의 묶음이 있을때 비교횟수 최소화
 
+# 매번 제일 작은 2개 비교가 필요하다
 
 # https://www.acmicpc.net/problem/1715
