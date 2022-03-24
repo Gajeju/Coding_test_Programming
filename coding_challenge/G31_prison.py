a75ee8aedd131d9aafd0f138909d4ed039f1bcdb
제주 제주 <qwert6603@naver.com>
2022-03-25 00:32:03 +0900

    코딩챌린지 2문제

--- coding_challenge/G31_prison.py
+++ coding_challenge/G31_prison.py
@@ -2,3 +2,23 @@
 # 소장은 죄수들이 서로 탈옥 모의를 시도하지 않도록 하기 위해 구역을 둘로 나누려고 합니다.
 # 죄수들끼리 서로 볼 수 있는 경우의 정보가 주어지고,
 # 같은 구역에 있는 죄수들은 서로 볼 수 없도록 하는 프로그램을 만드세요.
+import sys
+
+num, info_num = map(int, sys.stdin.readline().split())
+info_list = [list(map(int, sys.stdin.readline().split())) for _ in range(info_num)]
+
+group_a = []
+group_b = []
+
+for info in info_list:
+    if info[0] in group_a:
+        if info[1] in group_a:
+            print(0)
+            break
+        else:
+            group_b.append(info[1])
+    else:
+        group_a.append(info[0])
+        group_b.append(info[1])
+else:
+    print(1)
---
+++ coding_challenge/G32_recreation.py
@@ -0,0 +1,12 @@
+# 기쁨초등학교 수련회 현장입니다.
+# 특별 레크리에이션 강사인 빈이는 장기자랑이 시작되기 전, 학생들의 흥을 높이기 위해 게임을 시작했습니다.
+# 우선 학생들이 일렬로 줄을 섭니다.
+# 예를 들어 강사가 숫자 3과 7을 외치면 줄을 3번째에 선 학생부터 7번째에 선 학생까지 중에 몸무게가 가장 적은 학생이 앞으로 나옵니다.
+# 레크리에이션 강사가 숫자를 외칠 때 앞으로 나오는 학생의 몸무게를 출력하세요.
+import sys
+
+n = int(sys.stdin.readline())
+stu_weight = list(map(int, sys.stdin.readline().split()))
+start, end = map(int, sys.stdin.readline().split())
+
+print(min(stu_weight[start-1:end]))
