12952554829e34ffd7e7a40b88e77af388abb083
제주 제주 <qwert6603@naver.com>
2022-04-09 22:46:37 +0900

    BFS/DFS 기초

---
+++ coding_test/chapter05/G5-5.py
@@ -0,0 +1,15 @@
+# 반복적으로구현 n!
+
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return factorial_recursive(n - 1)
+
+print('반복적으로 구현 : ', factorial_iterative(5))
+print('재귀적으로 구현 : ', factorial_iterative(5))
